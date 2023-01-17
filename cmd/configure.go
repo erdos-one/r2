d990f8a1536a02fc6b74c720309184126575497a
William W. Marx <49069978+williamwmarx@users.noreply.github.com>
2023-01-17 05:27:10 +0000

    Read and write ~/.r2 configuration file

--- cmd/configure.go
+++ cmd/configure.go
@@ -3,14 +3,176 @@ package cmd
 import (
 	"fmt"
 	"log"
+	"os"
+	"path/filepath"
+	"regexp"
+	"sort"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
 
 type config struct {
 	Profile         string
+	AccountID       string
 	AccessKeyID     string
 	SecretAccessKey string
+}
+
+// ~/.r2 configuration file path
+func getConfigPath() string {
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		log.Fatal(err)
+	}
+	return filepath.Join(homeDir, ".r2")
+}
+
+var R2ConfigFile = getConfigPath()
+
+// Format configuration string
+func configString(c config) string {
+	return fmt.Sprintf(`[%s]
+account_id=%s
+access_key_id=%s
+secret_access_key=%s`, c.Profile, c.AccountID, c.AccessKeyID, c.SecretAccessKey)
+}
+
+// Get configuration credentials interactively
+func getCredentials(profile string) config {
+	var c config
+
+	// Get profile
+	if profile == "" {
+		// Get profile name
+		fmt.Print("Profile [default]: ")
+		fmt.Scanln(&profile)
+		if profile == "" {
+			profile = "default"
+		}
+	}
+	c.Profile = profile
+
+	// Get account ID
+	fmt.Print("Account ID: ")
+	fmt.Scanln(&c.AccountID)
+
+	// Get access key ID
+	fmt.Print("Access Key ID: ")
+	fmt.Scanln(&c.AccessKeyID)
+
+	// Get secret access key
+	fmt.Print("Secret Access Key: ")
+	fmt.Scanln(&c.SecretAccessKey)
+
+	return c
+}
+
+// Parse configuration file and return profiles
+func getConfig() map[string]config {
+	// Create configuration file if it doesn't exist
+	if _, err := os.Stat(R2ConfigFile); os.IsNotExist(err) {
+		f, err := os.Create(R2ConfigFile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer f.Close()
+
+		// Get credentials interactively and write to configuration file
+		writeConfig(getCredentials(""))
+	}
+
+	// Read configuration file
+	c, err := os.ReadFile(R2ConfigFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Remove empty lines
+	configString := regexp.MustCompile(`^\n$`).ReplaceAllString(string(c), "")
+
+	// Parse configuration file into profiles
+	var profiles = make(map[string]config)
+
+	profilesRe := regexp.MustCompile(`\[[\w\s\]=]+`)
+	for _, p := range profilesRe.FindAllString(configString, -1) {
+		// Strip all whitespace
+		p = regexp.MustCompile(`\s`).ReplaceAllString(p, "")
+
+		// Parse profiles
+		var profile config
+
+		// Get profile name
+		if regexp.MustCompile(`\[\w+\]`).MatchString(p) {
+			profile.Profile = regexp.MustCompile(`\[(\w+)\]`).FindAllStringSubmatch(p, -1)[0][1]
+		}
+
+		// Get account ID
+		if regexp.MustCompile(`account_id=\w+`).MatchString(p) {
+			profile.AccountID = regexp.MustCompile(`account_id=(\w+)`).FindAllStringSubmatch(p, -1)[0][1]
+		}
+
+		// Get access key ID
+		if regexp.MustCompile(`access_key_id=\w+`).MatchString(p) {
+			profile.AccessKeyID = regexp.MustCompile(`access_key_id=(\w+)`).FindAllStringSubmatch(p, -1)[0][1]
+		}
+
+		// Get secret access key
+		if regexp.MustCompile(`secret_access_key=\w+`).MatchString(p) {
+			profile.SecretAccessKey = regexp.MustCompile(`secret_access_key=(\w+)`).FindAllStringSubmatch(p, -1)[0][1]
+		}
+
+		profiles[profile.Profile] = profile
+	}
+
+	return profiles
+}
+
+// Write configuration to file
+func writeConfig(c config) {
+	// Read configuration file
+	profiles := getConfig()
+
+	// Add profile to configuration if all credentials are provided
+	if c.Profile != "" && c.AccountID != "" && c.AccessKeyID != "" && c.SecretAccessKey != "" {
+		profiles[c.Profile] = c
+	}
+
+	// Format profile strings and sort alphabetically (default profile is always first)
+	var configStrings []string
+	for _, p := range profiles {
+		if p.Profile != "default" {
+			configStrings = append(configStrings, configString(p))
+		}
+	}
+	sort.Slice(configStrings, func(i, j int) bool {
+		return strings.ToLower(configStrings[i]) < strings.ToLower(configStrings[j])
+	})
+	if _, ok := profiles["default"]; ok {
+		configStrings = append([]string{configString(profiles["default"])}, configStrings...)
+	}
+
+	// Write configuration to file
+	f, err := os.Create(R2ConfigFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+	_, err = f.WriteString(strings.Join(configStrings, "\n\n"))
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+// Handle configuration options
+func handleConfig(c ...config) {
+	// Check if configuration provided
+	if len(c) > 0 && c[0].AccountID != "" && c[0].AccessKeyID != "" && c[0].SecretAccessKey != "" {
+		writeConfig(c[0])
+	} else {
+		// If no configuration provided, get configuration interactively
+		writeConfig(getCredentials(""))
+	}
 }
 
 // configureCmd represents the configure command
@@ -48,70 +210,45 @@ To generate an API Token, follow Cloudflare's guide at:
 
 Be careful not to share your API Token credentials with anyone.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		// Store configuration options
+		// Parse configuration
 		var c config
+		var err error
 
 		// Get profile name
-		profileName, err := cmd.Flags().GetString("profile")
+		c.Profile, err = cmd.Flags().GetString("profile")
 		if err != nil {
 			log.Fatal(err)
 		}
-		c.Profile = profileName
+
+		// Get account ID
+		c.AccountID, err = cmd.Flags().GetString("account-id")
+		if err != nil {
+			log.Fatal(err)
+		}
 
 		// Get access key ID
-		accessKeyID, err := cmd.Flags().GetString("access-key-id")
+		c.AccessKeyID, err = cmd.Flags().GetString("access-key-id")
 		if err != nil {
 			log.Fatal(err)
 		}
-		c.AccessKeyID = accessKeyID
 
 		// Get secret access key
-		secretAccessKey, err := cmd.Flags().GetString("secret-access-key")
+		c.SecretAccessKey, err = cmd.Flags().GetString("secret-access-key")
 		if err != nil {
 			log.Fatal(err)
 		}
-		c.SecretAccessKey = secretAccessKey
 
-		// Parse configuration
-		if c == (config{}) {
-			// No flags passed, configure interactively
-			// Get profile name
-			fmt.Print("Profile [default]: ")
-			fmt.Scanln(&c.Profile)
-			if c.Profile == "" {
-				c.Profile = "default"
-			}
-
-			// Get access key ID
-			fmt.Print("Access Key ID: ")
-			fmt.Scanln(&c.AccessKeyID)
-
-			// Get secret access key
-			fmt.Print("Secret Access Key: ")
-			fmt.Scanln(&c.SecretAccessKey)
-		} else if c.Profile != "" && c.AccessKeyID == "" && c.SecretAccessKey == "" {
-			// Profile passed, but API credentials not passed
-			// Get access key ID
-			fmt.Print("Access Key ID: ")
-			fmt.Scanln(&c.AccessKeyID)
-
-			// Get secret access key
-			fmt.Print("Secret Access Key: ")
-			fmt.Scanln(&c.SecretAccessKey)
-		} else if c.Profile == "" && c.AccessKeyID != "" && c.SecretAccessKey != "" {
-			// Access key ID and secret access key passed, but profile not passed
-			// Set profile to default
-			c.Profile = "default"
-		} else if c.AccessKeyID == "" || c.SecretAccessKey == "" {
-			// Either access key ID or secret access key not passed
+		// Either access key ID or secret access key not passed but not both
+		if (c.AccessKeyID == "" && c.SecretAccessKey != "") || (c.AccessKeyID != "" && c.SecretAccessKey == "") {
 			log.Fatal(`Error: You must either provide both the access key ID and secret access key or
 neither to configure interactively.
 
 For more information, run:
   r2 help configure`)
+		} else {
+			// Interactively get credentials if no access key ID or secret access key provided
+			handleConfig(c)
 		}
-
-		fmt.Println(c)
 	},
 }
 
@@ -121,6 +258,7 @@ func init() {
 
 	// Add flags to the configure subcommand
 	configureCmd.Flags().String("profile", "", "Configure a named profile")
+	configureCmd.Flags().String("account-id", "", "R2 Account ID")
 	configureCmd.Flags().String("access-key-id", "", "R2 Access Key ID")
 	configureCmd.Flags().String("secret-access-key", "", "R2 Secret Access Key")
 }
